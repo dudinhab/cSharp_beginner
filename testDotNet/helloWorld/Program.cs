bfadb5eb33ba87ec60a5cc5c03b118a15739fed9
Maria Eduarda Barone <duda.barone@outlook.com>
2025-02-23 21:13:24 +0100

    changed hello word
    -Add string variable

--- testDotNet/helloWorld/Program.cs
+++ testDotNet/helloWorld/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+String firstName = "Maria";
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"I'm {firstName}!");
+Console.WriteLine("And this is my first C# Program!");
